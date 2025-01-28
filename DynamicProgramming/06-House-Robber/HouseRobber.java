15870ca57b39306704a865abd657c7e01a5938ad
HorcruxAshu <ashumanorkar@gmail.com>
2025-01-28 19:51:27 +0530

    Added house robber dp

---
+++ DynamicProgramming/06-House-Robber/HouseRobber.java
@@ -0,0 +1,15 @@
+// Link - https://leetcode.com/problems/house-robber/description/
+public class HouseRobber {
+    class Solution {
+        public int rob(int[] nums) {
+            int[] dp = new int[nums.length]; 
+            if(nums.length == 1) return nums[0];
+            dp[0]= nums[0];
+            dp[1] = Math.max(nums[0], nums[1]); //dp[i] indicates the maximum amt robbed til ith house.
+            for(int i = 2; i < nums.length; i++){
+                dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]); // dp[i] = Math.max(dp[i - 1] Not robbing the house, nums[i] + dp[i - 2] robbing the ith house.);
+            }
+            return dp[nums.length -1];
+        }
+    }
+}
