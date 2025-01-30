61bb03024bd720a90cb09db07431d104d3ba538d
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-01-30 21:51:08 +0000

    FrogJumpRecursiveSolution

---
+++ DynamicProgramming/08-Frog Jump/Java Sol/CPP Solution/01-ForgJumpRecursion.cpp
@@ -0,0 +1,77 @@
+/*
+Platform -> Leetcode.
+Problem Link -> https://leetcode.com/problems/frog-jump/description/
+Problem Statement:
+A frog is crossing a river. The river is divided into some number of units, and at each unit, there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
+
+Given a list of stones positions (in units) in sorted ascending order, determine if the frog can cross the river by landing on the last stone. Initially, the frog is on the first stone and assumes the first jump must be 1 unit.
+
+If the frog's last jump was k units, its next jump must be either k - 1, k, or k + 1 units. The frog can only jump in the forward direction.
+
+ 
+
+Example 1:
+
+Input: stones = [0,1,3,5,6,8,12,17]
+Output: true
+Explanation: The frog can jump to the last stone by jumping 1 unit to the 2nd stone, then 2 units to the 3rd stone, then 2 units to the 4th stone, then 3 units to the 6th stone, 4 units to the 7th stone, and 5 units to the 8th stone.
+Example 2:
+
+Input: stones = [0,1,2,3,4,8,9,11]
+Output: false
+Explanation: There is no way to jump to the last stone as the gap between the 5th and 6th stone is too large.
+ 
+
+Constraints:
+
+2 <= stones.length <= 2000
+0 <= stones[i] <= 231 - 1
+stones[0] == 0
+stones is sorted in a strictly increasing order.
+*/
+class Solution {
+public:
+    int totalStones;
+    unordered_map<int, int> mp;
+    int DynamicArray2d[2001][2001];
+    
+    bool solve(vector<int>& stones, int currentStoneIndex, int previousJump) {
+        if(currentStoneIndex == totalStones-1)
+            return true;
+        
+        bool result = false;
+        
+        if(DynamicArray2d[currentStoneIndex][previousJump] != -1)
+            return DynamicArray2d[currentStoneIndex][previousJump];
+        
+        for(int nextJump = previousJump-1; nextJump <= previousJump+1; nextJump++) {
+            
+            if(nextJump > 0) {
+                int next_stone = stones[currentStoneIndex] + nextJump;
+                
+                if(mp.find(next_stone) != mp.end()) {
+                    result = result || solve(stones, mp[next_stone], nextJump);
+                }
+            }
+            
+        }
+        
+        return DynamicArray2d[currentStoneIndex][previousJump] = result;
+        
+    }
+    
+    bool canCross(vector<int>& stones) {
+        
+        if(stones[1] != 1)
+            return false;
+        
+        totalStones = stones.size();
+        for(int i = 0; i<totalStones; i++) {
+            mp[stones[i]] = i;
+        }
+        
+        memset(DynamicArray2d, -1, sizeof(DynamicArray2d));
+        
+        return solve(stones, 0, 0);
+    }
+};
