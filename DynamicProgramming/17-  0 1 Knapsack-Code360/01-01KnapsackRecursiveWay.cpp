61b72570695294f4c7559d32ef42066f3cb8e10c
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-02-03 23:33:14 +0000

    Knapsak Recursive Way

--- DynamicProgramming/17-  0 1 Knapsack-Code360/01-01KnapsackRecursiveWay.cpp
+++ DynamicProgramming/17-  0 1 Knapsack-Code360/01-01KnapsackRecursiveWay.cpp
@@ -27,3 +27,33 @@ Sample Output:
 Time Complexity -> 
 Space Complexity -> 
 */
+
+
+#include <bits/stdc++.h> 
+
+int solve (vector<int> weight, vector<int> value, int index, int capacity){
+	//Base case 
+	//If only 1 item to steal, then just compare its weight with knapsack capacity
+
+	if(index==0) {
+		if(weight[0] <= capacity) {
+			return value[0];
+		} else {
+			return 0;
+		}
+	}
+
+	int include=0;
+	if(weight[index]<=capacity) {
+		include = value[index] + solve(weight, value, index-1, capacity-weight[index]);
+	} 
+	int exclude = 0+solve(weight, value,index-1, capacity);
+	int ans = max(include, exclude);
+	return ans;
+}
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+	// Write your code here
+	return solve(weight, value, n-1, maxWeight);
+	
+}
