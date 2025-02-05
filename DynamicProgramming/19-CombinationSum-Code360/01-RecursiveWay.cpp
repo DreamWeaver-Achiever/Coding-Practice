7b1ef100ed0ebd623c0ee06bcacfe7c7cb7d7e47
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-02-05 20:54:27 +0000

    Added Time And Space Complexities

--- DynamicProgramming/19-CombinationSum-Code360/01-RecursiveWay.cpp
+++ DynamicProgramming/19-CombinationSum-Code360/01-RecursiveWay.cpp
@@ -1,7 +1,7 @@
 /*
 Platform -> Code360
 Problem Link -> 
-Problem Statement -> 
+Problem Statement -> https://www.naukri.com/code360/problems/number-of-ways_3755252?leftPanelTab=0&leftPanelTabValue=PROBLEM
 You are given an array of distinct integers and you have to tell how many different ways of selecting the elements from the array are there such that the sum of chosen elements is equal to the target number tar.
 
 Note: Two ways are considered the same if for every index the contents of both the ways are equal example way1=[1,2,3,1] and way2= [1,2,3,1] both way1 and way 2 are the same.
@@ -22,8 +22,8 @@ If N = 3 and tar = 5 and array elements are [1,2,5] then the number of possible 
 Hence the output will be 9.
 */
 /*
-Time Complexity -> 
-Space Complexity -> 
+Time Complexity -> Exponential.
+Space Complexity -> O(target)
 */
 #include <bits/stdc++.h> 
 int findWays(vector<int> &num, int target)
