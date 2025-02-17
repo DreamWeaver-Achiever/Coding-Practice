2c6a8211930496b7c45666d551949176983de4e8
Sakshi M <sakm2160@gmail.com>
2025-02-17 18:55:06 +0000

    Space optimization

---
+++ DynamicProgramming/29-Minimum-Sideway-Jumps-Leetcode/04-SpaceOptimization.cpp
@@ -0,0 +1,60 @@
+/*
+Platform : Leetcode
+Problem Link: https://leetcode.com/problems/minimum-sideway-jumps/description/
+Problem Statement : 
+There is a 3 lane road of length n that consists of n + 1 points labeled from 0 to n. A frog starts at point 0 in the second lane and wants to jump to point n. However, there could be obstacles along the way.
+
+You are given an array obstacles of length n + 1 where each obstacles[i] (ranging from 0 to 3) describes an obstacle on the lane obstacles[i] at point i. If obstacles[i] == 0, there are no obstacles at point i. There will be at most one obstacle in the 3 lanes at each point.
+
+For example, if obstacles[2] == 1, then there is an obstacle on lane 1 at point 2.
+The frog can only travel from point i to point i + 1 on the same lane if there is not an obstacle on the lane at point i + 1. To avoid obstacles, the frog can also perform a side jump to jump to another lane (even if they are not adjacent) at the same point if there is no obstacle on the new lane.
+
+For example, the frog can jump from lane 3 at point 3 to lane 1 at point 3.
+Return the minimum number of side jumps the frog needs to reach any lane at point n starting from lane 2 at point 0.
+
+Note: There will be no obstacles on points 0 and n.
+
+*/
+
+/*
+Time Complexity -> O(3 * n) which simplifies to O(n).
+Space Complexity -> O(1).
+*/
+
+class Solution {
+    public:
+        int minSideJumps(vector<int>& obstacles) {
+    
+            int n = obstacles.size() - 1; // as obstacles+1 is the length 
+    
+            vector<int> current(4, INT_MAX);
+            vector<int> next(4, INT_MAX);
+    
+            // Base cases
+            next[0] = 0;
+            next[1] = 0;
+            next[2] = 0;
+            next[3] = 0;
+    
+            // Sideway jump logic
+            for (int currentPosition = n - 1; currentPosition >= 0; currentPosition--) {
+                for (int currentLane = 1; currentLane <= 3; currentLane++) {
+                    if (obstacles[currentPosition + 1] != currentLane) {
+                        current[currentLane] = next[currentLane];
+                    } else {
+                        // sideways jump
+                        int ans = INT_MAX;
+                        for (int i = 1; i <= 3; i++) {
+                            if ((currentLane != i) && (obstacles[currentPosition] != i)) {
+                                ans = min(ans, 1 + next[i]);
+                            } 
+                            current[currentLane]=ans;
+                        }
+                        next=current;
+                    }
+                }
+            } 
+            return min(next[2], min(1+next[1], 1+next[3]));
+        }
+    }
+    ;
