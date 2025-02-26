0b703a0b24c1b94ae5e0971a68c027009ad9dbf6
Sakshi M <sakm2160@gmail.com>
2025-02-26 16:58:12 +0000

    DP With Binary Search

--- DynamicProgramming/30-LongestIncreasingSubsequence/05-DPWithBinarySearch.cpp
+++ DynamicProgramming/30-LongestIncreasingSubsequence/05-DPWithBinarySearch.cpp
@@ -23,6 +23,69 @@ Constraints:
 */
 
 /*
-Time complexity -> 
-Space complexity -> 
+Time complexity -> O(nlog n)
+Space complexity -> O(n)
 */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+
+class Solution {
+  public: 
+    int lis(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        if(n==0) {
+            return 0;
+        } 
+        
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for(int i=1; i<n; i++) {
+            if(arr[i]>ans.back()) {
+                ans.push_back(arr[i]);
+            } else {
+                //find index of just largest elements in ans
+                int index = lower_bound(ans.begin(), ans.end(), arr[i]) - ans.begin();
+                ans[index]=arr[i];
+            }
+        } 
+        return ans.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
