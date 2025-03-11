8da93d9ac6e03c6b5a62fe52ffc8cebdcb7168a7
Sakshi M <sakm2160@gmail.com>
2025-03-11 18:19:40 +0000

    Problem Statement

---
+++ DynamicProgramming/35-PartitionEqualSubsetSum-Gfg/03-.cpp
@@ -0,0 +1,12 @@
+/*
+Platform: Gfg
+Problem Link: 
+Problem Statement:
+Given an array arr[], return true if it can be partitioned into two subsets such that the sum of elements in both parts is the same, otherwise, false.
+Note: Each element is present in either the first subset or the second subset, but not in both.
+*/
+
+/* Complexities:
+Time Complexity:
+Space Complexity:
+*/
