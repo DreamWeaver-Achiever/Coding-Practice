b7a1662ad4af3a56c3dfce80bb5513183d157659
Sakshi M <sakm2160@gmail.com>
2025-03-14 21:35:12 +0000

    Space Optimized Solution

---
+++ DynamicProgramming/36-MinimumSwapsToMakeSubsequencesIncreasing/04-SpaceOptimized.cpp
@@ -0,0 +1,63 @@
+/*
+Platform-> Leetcode.
+Problem Link -> 
+Problem Statement -> 
+You are given two integer arrays of the same length nums1 and nums2. In one operation, you are allowed to swap nums1[i] with nums2[i].
+For example, if nums1 = [1,2,3,8], and nums2 = [5,6,7,4], you can swap the element at i = 3 to obtain nums1 = [1,2,3,4] and nums2 = [5,6,7,8].
+Return the minimum number of needed operations to make nums1 and nums2 strictly increasing. The test cases are generated so that the given input always makes it possible.
+An array arr is strictly increasing if and only if arr[0] < arr[1] < arr[2] < ... < arr[arr.length - 1]. 
+*/
+
+/*
+Complexities -> 
+Time Complexity: O(n).
+Space Complexity: O(1).
+*/
+
+class Solution {
+    public:
+        int minSwap(vector<int>& nums1, vector<int>& nums2) {
+            nums1.insert(nums1.begin(), -1);
+            nums2.insert(nums2.begin(), -1);
+            int n = nums1.size();
+    
+            int swapped = 0; 
+            int noSwapped = 0;
+            int currentSwap = 0;
+            int currentNoSwap = 0; 
+    
+            for(int index=n-1; index>=1; index--) {
+                for(int swapcase=1; swapcase>=0; swapcase--) {
+                    // If elements of an array are not yet computed then calculate them recursively.
+                    int ans = INT_MAX;
+                    int previous1 = nums1[index-1];
+                    int previous2 = nums2[index-1];
+    
+                    //Check if indexes are already swapped.
+                    if(swapcase) {
+                        swap(previous1, previous2);
+                    }
+    
+                    //No-swap 
+                    if(nums1[index]>previous1 && nums2[index]>previous2) { 
+                        ans = noSwapped; 
+                        } 
+    
+                    //Swap 
+                    if(nums1[index]>previous2 && nums2[index]>previous1) { 
+                        ans = min(ans, 1+swapped); 
+                        } 
+                    
+                    if(swapcase) {
+                        currentSwap = ans;
+                    } else {
+                        currentNoSwap = ans;
+                    }
+                }
+                swapped = currentSwap;
+                noSwapped = currentNoSwap;
+            } 
+            return (swapped, noSwapped);
+    
+        }
+    };
