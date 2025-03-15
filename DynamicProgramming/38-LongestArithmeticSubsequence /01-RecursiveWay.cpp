15421924b4c09ee0b126a9e26ab130aba3199aee
Sakshi M <sakm2160@gmail.com>
2025-03-15 18:29:17 +0000

    Recursive Way

---
+++ DynamicProgramming/38-LongestArithmeticSubsequence /01-RecursiveWay.cpp
@@ -0,0 +1,13 @@
+/*
+Platform -> gfg
+Problem Link -> https://www.geeksforgeeks.org/problems/longest-arithmetic-progression1019/1
+Problem Statment -> 
+Given an array arr[] of sorted and distinct positive integers. The task is to find the length of the longest arithmetic progression in a given array.
+
+Note: A sequence seq is an arithmetic progression if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+*/
+
+/* Complexities ->  
+Time Complexity -> 
+Space Complexity -> 
+*/
