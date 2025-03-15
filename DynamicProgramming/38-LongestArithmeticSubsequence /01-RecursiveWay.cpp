0d5551b64258a4e9b61946b6edef6602c73363b3
Sakshi M <sakm2160@gmail.com>
2025-03-15 18:42:35 +0000

    Recursive Way Code

--- DynamicProgramming/38-LongestArithmeticSubsequence /01-RecursiveWay.cpp
+++ DynamicProgramming/38-LongestArithmeticSubsequence /01-RecursiveWay.cpp
@@ -8,6 +8,75 @@ Note: A sequence seq is an arithmetic progression if seq[i + 1] - seq[i] are all
 */
 
 /* Complexities ->  
-Time Complexity -> 
-Space Complexity -> 
+Time Complexity -> O(n^3).
+Space Complexity -> O(n).
 */
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+   int calculateLongestAP(int index, int diff,vector<int>& arr) {
+       //Backward check
+       if(index<0) {
+           return 0;
+       } 
+       int ans=0;
+       for(int j=index-1;j>=0; j--) {
+           if(arr[index]-arr[j]==diff) {
+               ans = max(ans, 1+calculateLongestAP(j, diff, arr));
+           }
+       } 
+       return ans;
+   }
+    int lengthOfLongestAP(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        //Base case
+        if(n<=2) {
+            return n;
+        }
+        int ans=0;
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                ans = max(ans, 2+calculateLongestAP(i, arr[j]-arr[i], arr));
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.lengthOfLongestAP(arr);
+        cout << ans << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
