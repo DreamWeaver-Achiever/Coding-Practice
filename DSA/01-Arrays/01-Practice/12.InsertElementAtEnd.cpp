1e409a50cea619f0585c23135d7d538ea75d91ff
Sakshi M <sakm2160@gmail.com>
2025-04-15 18:36:14 +0000

    Insert element at the end of an array

--- DSA/01-Arrays/01-Practice/12.InsertElementAtEnd.cpp
+++ DSA/01-Arrays/01-Practice/12.InsertElementAtEnd.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-using namespcae std;
+#include <vector>
+using namespace std;
 int main() {
-    cout<<"Insert element at the end of an array: ";
+    cout<<"Insert element at the end of an array: "<<endl;
+    vector<int> arr = {10, 20, 30, 40};
+    int element = 50;
+    arr.push_back(element);
+    cout<<"Modified array: ";
+    for(int i=0; i<arr.size(); i++) {
+        cout<<arr[i]<<" ";
+    } 
+    return 0;
     
 }
